5e2c773bae9aa5a4291ff520503645fb828b61f8
Riyazuddin <riyazuddin@Riyazuddins-MacBook-Pro.local>
2023-01-19 18:53:15 +0530

    helm

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -76,7 +76,7 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	resp := response{
 		URL:             body.URL,
-		Short:           id,
+		Short:           "localhost:3000/" + id,
 		Expiry:          body.Expiry,
 		XRateRemaining:  10,
 		XRateLimitReset: 30,
