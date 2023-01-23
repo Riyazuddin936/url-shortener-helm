7e4cff4915b11709a4eb44c511d2832f0335e1b3
Riyazuddin936 <49946667+Riyazuddin936@users.noreply.github.com>
2023-01-23 16:38:41 +0530

    Update main.go

--- api/main.go
+++ api/main.go
@@ -12,7 +12,7 @@ import (
 
 func setupRoutes(app *fiber.App) {
 	app.Get("/:url", routes.ResolveURL)
-	app.Post("/api/v1", routes.ShortenURL)
+	app.Post("/api/v2", routes.ShortenURL)
 }
 
 func main() {
