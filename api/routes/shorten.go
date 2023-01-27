39b0e27f2ccbb1f9c0a2203d3bd8b88647ba0c3b
Riyazuddin936 <49946667+Riyazuddin936@users.noreply.github.com>
2023-01-27 15:20:31 +0530

    Update shorten.go

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -76,7 +76,7 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	resp := response{
 		URL:             body.URL,
-		Short:           "localhost:4030/" + id,
+		Short:           "localhost:4050/" + id,
 		Expiry:          body.Expiry,
 		XRateRemaining:  10,
 		XRateLimitReset: 30,
